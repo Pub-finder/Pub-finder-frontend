49a6076f01230ee268dac233104518728052b9bd
Linus Silfver <linus.silfver@gmail.com>
2024-09-10 22:28:58 +0200

    test 7 new snapPoint

--- src/bottomBar/BottomSheet/BottomSheet.jsx
+++ src/bottomBar/BottomSheet/BottomSheet.jsx
@@ -8,7 +8,7 @@ export default function BottomSheet() {
     <>
       <Sheet
         isOpen={true}
-        snapPoints={[0.6, 186, 58]}
+        snapPoints={[0.6, 188, 55]}
         initialSnap={1}
         onClose={() => {
           console.log('Sheet closed');
