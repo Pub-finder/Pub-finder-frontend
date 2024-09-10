2a464123d3523bedf1a3ef35979a3c1ba9ce5c09
Linus Silfver <linus.silfver@gmail.com>
2024-09-10 13:34:49 +0200

    fixed a small error

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -64,7 +64,7 @@ export default function Map() {
       setIsLoading(false);
     } catch (error) {
       console.log("Unable to retrieve user location:", error);
-      setPositionError(true);
+      setIsError(true);
       setIsLoading(false);
     }
   };
