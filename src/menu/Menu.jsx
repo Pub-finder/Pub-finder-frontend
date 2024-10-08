909ba0f79f86cb37378c1ddc2589db5f837360ec
Linus Silfver <linus.silfver@gmail.com>
2024-10-08 14:15:15 +0200

    added the menu

---
+++ src/menu/Menu.jsx
@@ -0,0 +1,62 @@
+import {useState} from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { motion, Variants } from "framer-motion"
+import { menuVariants, itemVariants } from "./variants"
+import styles from './style.module.scss';
+import { Link } from "react-router-dom";
+import { authenticatedMenu, unauthenticatedMenu } from "./menus";
+
+export default function DropdownMenu() {
+    const authenticated = true
+    const [isOpen, setIsOpen] = useState(false);
+    const menu = authenticated ? authenticatedMenu : unauthenticatedMenu;
+
+    return (
+        <div className={styles.menuContainer}>
+            <section>
+                <motion.button
+                    whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 0.2 },
+                    }}
+                    whileTap={{ scale: 0.8 }}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <FaUserCircle size={35} />
+                </motion.button>
+
+                <motion.ul
+                    initial="closed"
+                    animate={isOpen ? 'open' : 'closed'}
+                    variants={menuVariants}
+                    style={{ overflow: 'hidden' }}
+                >
+                    {menu.map((variant) => (
+                        <motion.li
+                            key={variant.id}
+                            variants={itemVariants}
+                            whileHover={{
+                                scale: 1.1,
+                                transition: { duration: 0.2 },
+                            }}
+                            whileTap={{ scale: 0.8 }}
+                        >
+                            {variant.name !== "Logout" ? (
+                                <Link to={variant.link} className={styles.link}>
+                                    {variant.svg}
+                                    {variant.name}
+                                </Link>
+                            ) : (
+                                <span
+                                    onClick={() => console.log("logout")} className={styles.logout}>
+                                    {variant.svg}
+                                    {variant.name}
+                                </span>
+                            )}
+                        </motion.li>
+                    ))}
+               </motion.ul>
+            </section>
+        </div>
+    );
+}
