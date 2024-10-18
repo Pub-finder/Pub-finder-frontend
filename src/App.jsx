b8289b52667a1526448c059bd8d3d583c1ba530f
Linus Silfver <linus.silfver@gmail.com>
2024-10-18 15:56:55 +0200

    alot of changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { React } from "react";
 import './App.css';
-import StartPage from "./startPage/StartPage";
+import MapPage from "./mapPage/MapPage";
 import { Routes, Route } from "react-router-dom";
 import Register from "./auth/register/Register";
 import Login from "./auth/login/Login";
@@ -9,12 +9,11 @@ import UserReviews from "./reviews/userReviews/UserReviews";
 import Menu from "./menu/Menu";
 
 export default function App() {
-    console.log("userId: ", localStorage.getItem("userId"));
   return (
     <div>
         <Menu />
       <Routes>
-        <Route path="/" element={<StartPage />} />
+        <Route path="/" element={<MapPage />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/visitedPubs" element={<VisitedPubs />} />
--- src/visitedPubs/VisitedPubs.jsx
+++ src/visitedPubs/VisitedPubs.jsx
@@ -9,14 +9,14 @@ import VisitedPub from "./VisitedPub";
 export default function VisitedPubs() {
     const username = localStorage.getItem("username");
     const userId = localStorage.getItem("userId");
-    const { data: visitedPubs = [], isLoading, error, isError, isSuccess } = useGetVisitedPubsQuery(userId ? userId : skipToken);
+    const { data: visitedPubs = [], isLoading, isError, isSuccess } = useGetVisitedPubsQuery(userId ? userId : skipToken);
 
     return (
         <div className={styles.visitedPubs}>
             <h2>Pubs that {username} has visited</h2>
             {isLoading && <Loader />}
-            {isError && (error?.originalStatus !== 404) && <p className={styles.errorMsg}>There was an unexpected error. Unable to display Visited Pubs.</p>}
-            {(isSuccess && visitedPubs.length === 0) || (error?.originalStatus === 404) && (
+            {isError && <p className={styles.errorMsg}>There was an unexpected error. Unable to display Visited Pubs.</p>}
+            {(isSuccess && visitedPubs.length === 0) && (
                 <p className={styles.errorMsg}>You have not visited any pubs yet.</p>
             )}
             {isSuccess && (
